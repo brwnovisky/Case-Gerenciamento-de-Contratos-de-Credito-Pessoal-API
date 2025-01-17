e3ff5fbb0c9121e511f9104bda21ac4cfdbec4b3
brwnovisky <brunoluizalvesn@gmail.com>
2025-01-17 19:41:37 -0300

    GCCP_API_3 - Set settings.py configuration.

--- GCCP_API/gccp_api_root/settings.py
+++ GCCP_API/gccp_api_root/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gccp_api_rest',
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
+    'psycopg2',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'gccp_api_root.urls'
@@ -121,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+CORS_ALLOW_ORIGINS = [
+    'http://localhost:8000',
+]
